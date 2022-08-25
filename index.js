4afc6d069c644821fd0f0a321a9037a999b06b44
Meshari <meshari.s@homeofpmg.com>
2022-08-25 08:04:41 +0400

    content security policy fix

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-require("dotenv").config();
+require('dotenv').config();
 console.clear();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const api = require("./api");
-const { errorHandler, notFound, databaseStatus } = require("./middlewares");
-const connect = require("./lib/database");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const joi = require('joi');
+const helmet = require('helmet');
+const api = require('./api');
+const { errorHandler, notFound, databaseStatus } = require('./middlewares');
+const connect = require('./lib/database');
+const cookieParser = require('cookie-parser');
 
 // Configs
-const { CLIENT, WEB_SERVER, META, CORS } = require("./lib/configs");
+const { CLIENT, WEB_SERVER, META, CORS } = require('./lib/configs');
 
 const app = express();
 
 //Database
 connect()
   .then((client) => {
-    app.set("database", true);
-    app.set("database-client", client);
+    app.set('database', true);
+    app.set('database-client', client);
   })
   .catch((err) => {
     console.log(err);
-    app.set("database", false);
+    app.set('database', false);
   });
 
 // Middlewares
-app.set("trust proxy", 1);
-app.use(morgan("dev"));
+app.set('trust proxy', 1);
+app.use(morgan('dev'));
 app.use(
   cors({
     origin: [CORS.ALLOWED],
     credentials: true,
-    allowedHeaders: "Content-Type,Authorization",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  })
+    allowedHeaders: 'Content-Type,Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  }),
 );
 app.use(cookieParser());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
 app.use(express.json());
 
+const functions = {
+  string: () => {
+    return 'string';
+  },
+  alphanum: () => {
+    return 'alphanum';
+  },
+  number: () => {
+    return 'number';
+  },
+};
+
 // API route
+app.get('/:key', (req, res, next) => {
+  const schema = joi[req.params.key]();
+  const schema2 = joi.number();
+  console.log(schema);
+
+  res.send('ok');
+});
 app.use(`/${META.API_VERSION}`, databaseStatus, api);
 
 app.listen(WEB_SERVER.PORT, () => {
